07c97826b6a105233f7a50baf4082e9f8bd06581
William Blankenship <git@blankenship.io>
2020-07-06 16:50:33 -0600

    simplify GET /changesets and add comments

--- src/routes/changesets/get.js
+++ src/routes/changesets/get.js
@@ -1,36 +1,45 @@
 const log = require('../../log')
-const neo = require('neo-async')
 const sql = require('../../sql')
 
 module.exports = (pool) => (request, response, next) => {
   log.info({ query: request.query }, 'got request')
   const { name } = request.query
 
-  let result = {}
-  neo.waterfall([
-    (cb) => {
-      if (name) {
-        return pool.query(sql.select.changesetsByName, [name], cb)
-      }
-      pool.query(sql.select.changesets, cb)
-    },
-    (res, cb) => {
-      if (name) {
-        result = { changesets: res.rows }
-      } else {
-        result = { changesets: res.rows.map(v => v.name) }
-      }
-      cb()
-    }
-  ], err => {
+  // This endpoint accepts two forms:
+  //   * returning all changesets
+  //   * fetching all changesets by name
+  // This next block picks which query to run based on whether or not we
+  // received a query string from the user. For the first case we only return
+  // the unique names we know of to reduce the amount of data being sent over
+  // the wire.
+  let query, args
+  if (name) {
+    query = sql.select.changesetsByName
+    args = [name]
+  } else {
+    query = sql.select.changesets
+    args = []
+  }
+  pool.query(query, args, (err, res) => {
+    // If we got an error back from the database, generate a 500 and abort the
+    // request.
     if (err) {
       log.error({ err })
       err.statusCode = 500
       return next(err)
     }
+
     response.header('content-type', 'application/json')
     response.status(201)
-    response.send(result)
+    // Format the result based on the same logic as above
+    // Note: Instead of just returning res.rows, we add it as the value of the
+    // changesets key because a valid application/json payload can't have an
+    // array as the top level object.
+    if (name) {
+      response.send({ changesets: res.rows })
+    } else {
+      response.send({ changesets: res.rows.map(v => v.name) })
+    }
     next()
   })
 }
