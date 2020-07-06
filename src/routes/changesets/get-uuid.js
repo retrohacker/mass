d627eca506bb36d380866989b1360a030b806a57
William Blankenship <git@blankenship.io>
2020-07-06 16:42:37 -0600

    simplify get-uuid and add comments

--- src/routes/changesets/get-uuid.js
+++ src/routes/changesets/get-uuid.js
@@ -1,5 +1,4 @@
 const log = require('../../log')
-const neo = require('neo-async')
 const sql = require('../../sql')
 const isUUID = /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
 
@@ -20,30 +19,31 @@ module.exports = (pool) => (request, response, next) => {
     return invalid('expected valid v4 uuid string')
   }
 
-  let result
-  neo.waterfall([
-    (cb) => {
-      log.info({ uuid }, 'query')
-      pool.query(sql.select.changeset, [uuid], cb)
-    },
-    (res, cb) => {
-      if (res.rows.length === 0) {
-        const err = new Error(`No changeset with uuid: ${uuid}`)
-        err.statusCode = 404
-        return cb(err)
-      }
-      result = res.rows[0]
-      cb()
-    }
-  ], err => {
+  log.info({ uuid }, 'query')
+  pool.query(sql.select.changeset, [uuid], (err, res) => {
+    // If we got an error back from our query, we are going to pass it to the
+    // client, but want to send it back as a 500
     if (err) {
       log.error({ err })
       err.statusCode = 500
+    }
+    // If we got an error back from the database that takes precedent over
+    // generating a 404. If the request was a success and we didn't get any
+    // rows back for our query, generate a 404.
+    if (!err && res.rows.length === 0) {
+      err = new Error(`No changeset with uuid: ${uuid}`)
+      err.statusCode = 404
+    }
+    // If we got an error back from the database, or generated a 404, send that
+    // back to the client and abort the request
+    if (err) {
       return next(err)
     }
+    // If we didn't run into an error, go ahead and pass the returned row
+    // directly back to the client
     response.header('content-type', 'application/json')
     response.status(200)
-    response.send(result)
+    response.send(res.rows[0])
     next()
   })
 }
