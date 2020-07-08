573946c345b4bab33d427538160c972e9fae2ea6
William Blankenship <git@blankenship.io>
2020-07-07 18:25:40 -0600

    lint tests

--- tests/changesets.get.test.js
+++ tests/changesets.get.test.js
@@ -1,106 +1,103 @@
-const test = require('ava');
-const port = require('get-port');
-const { promisify } = require('util');
-const server = promisify(require("../src/index.js"));
-const got = require("got");
-const uuid = require('uuid');
+const test = require('ava')
+const port = require('get-port')
+const { promisify } = require('util')
+const server = promisify(require('../src/index.js'))
+const got = require('got')
+const uuid = require('uuid')
 const config = () => ({
-  "server": {
-    "listen": [8000, "0.0.0.0"]
+  server: {
+    listen: [8000, '0.0.0.0']
   },
-  "db": {
-    "user": "mart",
-    "host": "localhost",
-    "database": "mass",
-    "port": 26257
+  db: {
+    user: 'mart',
+    host: 'localhost',
+    database: 'mass',
+    port: 26257
   }
 })
 
 const getServer = async t => {
-  const c = config();
-  const p = await port();
-  c.server.listen = [p, "0.0.0.0"];
-  const s = await server(c);
-  t.context.port = p;
-  t.context.server = s;
-  return [p, s];
+  const c = config()
+  const p = await port()
+  c.server.listen = [p, '0.0.0.0']
+  const s = await server(c)
+  t.context.port = p
+  t.context.server = s
+  return [p, s]
 }
 
 const body = () => ({
   name: `${(Math.random() * 1000000) | 0}`,
-  image: "fizzbuzz",
-  stakeholders: ["beep", "boop"]
-});
+  image: 'fizzbuzz',
+  stakeholders: ['beep', 'boop']
+})
 
 test.beforeEach(async t => {
-  const [p] = await getServer(t);
-  const b = body();
+  const [p] = await getServer(t)
+  const b = body()
   const resp = await got.post(`http://127.0.0.1:${p}/changesets`, {
     throwHttpErrors: false,
     json: b
-  }).json();
-  t.not(resp.uuid, undefined, "got uuid back");
-  t.context.uuid = resp.uuid;
-  t.context.body = b;
-});
+  }).json()
+  t.not(resp.uuid, undefined, 'got uuid back')
+  t.context.uuid = resp.uuid
+  t.context.body = b
+})
 
 test.afterEach(async t => {
-  await promisify(t.context.server.close);
-});
+  await promisify(t.context.server.close)
+})
 
-test("server should return uuid on payload", async t => {
-  t.plan(1);
-  const { uuid, body } = t.context;
-  const p = t.context.port;
-  resp = await got.get(`http://127.0.0.1:${p}/changesets/${uuid}`, {
-    throwHttpErrors: false,
-  }).json();
-  t.deepEqual(resp, body, "get returns body back after post")
-});
+test('server should return uuid on payload', async t => {
+  t.plan(1)
+  const { uuid, body } = t.context
+  const p = t.context.port
+  const resp = await got.get(`http://127.0.0.1:${p}/changesets/${uuid}`, {
+    throwHttpErrors: false
+  }).json()
+  t.deepEqual(resp, body, 'get returns body back after post')
+})
 
-test("server should 400 on non-existant uuid", async t => {
-  t.plan(1);
-  const { body } = t.context;
-  const p = t.context.port;
-  resp = await got.get(`http://127.0.0.1:${p}/changesets/${uuid.v4()}`, {
-    throwHttpErrors: false,
-  });
+test('server should 400 on non-existant uuid', async t => {
+  t.plan(1)
+  const p = t.context.port
+  const resp = await got.get(`http://127.0.0.1:${p}/changesets/${uuid.v4()}`, {
+    throwHttpErrors: false
+  })
   t.is(resp.statusCode, 404)
-});
+})
 
-test("server should error on invalid uuid string", async t => {
-  t.plan(1);
-  const { body } = t.context;
-  const p = t.context.port;
-  resp = await got.get(`http://127.0.0.1:${p}/changesets/foobar`, {
-    throwHttpErrors: false,
-  });
-  t.is(resp.statusCode, 400);
-});
+test('server should error on invalid uuid string', async t => {
+  t.plan(1)
+  const p = t.context.port
+  const resp = await got.get(`http://127.0.0.1:${p}/changesets/foobar`, {
+    throwHttpErrors: false
+  })
+  t.is(resp.statusCode, 400)
+})
 
-test("server should return array on get", async t => {
-  t.plan(2);
-  const { body } = t.context;
-  const p = t.context.port;
-  resp = await got.get(`http://127.0.0.1:${p}/changesets`, {
-    throwHttpErrors: false,
-  }).json();
-  t.assert(Array.isArray(resp.changesets), 'changesets is an array');
-  t.assert(resp.changesets.length > 0, 'changesets has at least one element');
-});
+test('server should return array on get', async t => {
+  t.plan(2)
+  const p = t.context.port
+  const resp = await got.get(`http://127.0.0.1:${p}/changesets`, {
+    throwHttpErrors: false
+  }).json()
+  t.assert(Array.isArray(resp.changesets), 'changesets is an array')
+  t.assert(resp.changesets.length > 0, 'changesets has at least one element')
+})
 
-test("server should return changesets matching name", async t => {
-  t.plan(5);
-  const { body, uuid } = t.context;
-  const p = t.context.port;
-  resp = await got.get(`http://127.0.0.1:${p}/changesets?name=${body.name}`, {
-    throwHttpErrors: false,
-  }).json();
-  t.assert(Array.isArray(resp.changesets), 'changesets is an array');
-  t.assert(resp.changesets.length > 0, 'changesets has at least one element');
-  t.truthy(resp.changesets[0].created, 'changeset includes timestamp');
-  t.truthy(resp.changesets[0].uuid, 'changeset includes uuid');
-  body.uuid = uuid;
-  body.created = resp.changesets[0].created;
-  t.deepEqual({ changesets: [ body ]}, resp);
-});
+test('server should return changesets matching name', async t => {
+  t.plan(5)
+  const { body, uuid } = t.context
+  const p = t.context.port
+  const resp = await got.get(`http://127.0.0.1:${p}/changesets?name=${body.name}`, {
+    throwHttpErrors: false
+  }).json()
+  t.assert(Array.isArray(resp.changesets), 'changesets is an array')
+  t.assert(resp.changesets.length > 0, 'changesets has at least one element')
+  t.truthy(resp.changesets[0].created, 'changeset includes timestamp')
+  t.truthy(resp.changesets[0].uuid, 'changeset includes uuid')
+  body.uuid = uuid
+  body.created = resp.changesets[0].created
+  t.deepEqual({ changesets: [body] }, resp)
+})
--- tests/changesets.post.test.js
+++ tests/changesets.post.test.js
@@ -1,137 +1,137 @@
-const test = require('ava');
-const port = require('get-port');
-const { promisify } = require('util');
-const server = promisify(require("../src/index.js"));
-const got = require("got");
+const test = require('ava')
+const port = require('get-port')
+const { promisify } = require('util')
+const server = promisify(require('../src/index.js'))
+const got = require('got')
 const config = () => ({
-  "server": {
-    "listen": [8000, "0.0.0.0"]
+  server: {
+    listen: [8000, '0.0.0.0']
   },
-  "db": {
-    "user": "mart",
-    "host": "localhost",
-    "database": "mass",
-    "port": 26257
+  db: {
+    user: 'mart',
+    host: 'localhost',
+    database: 'mass',
+    port: 26257
   }
 })
 
 const getServer = async t => {
-  const c = config();
-  const p = await port();
-  c.server.listen = [p, "0.0.0.0"];
-  const s = await server(c);
+  const c = config()
+  const p = await port()
+  c.server.listen = [p, '0.0.0.0']
+  const s = await server(c)
   t.teardown(async () => {
-    await promisify(s.close);
+    await promisify(s.close)
   })
-  return [p, s];
+  return [p, s]
 }
 
 const body = () => ({
-  name: "foobar",
-  image: "fizzbuzz",
-  stakeholders: ["beep", "boop"]
-});
+  name: 'foobar',
+  image: 'fizzbuzz',
+  stakeholders: ['beep', 'boop']
+})
 
-test("server should 400 on missing body", async t => {
-  const [p] = await getServer(t);
+test('server should 400 on missing body', async t => {
+  const [p] = await getServer(t)
   const resp = await got.post(`http://127.0.0.1:${p}/changesets`, {
     throwHttpErrors: false
-  }).json();
-  t.plan(1);
-  t.is(400, resp.statusCode);
-});
+  }).json()
+  t.plan(1)
+  t.is(400, resp.statusCode)
+})
 
-test("server should 400 on missing name", async t => {
-  const [p] = await getServer(t);
-  const b = body();
-  delete b.name;
+test('server should 400 on missing name', async t => {
+  const [p] = await getServer(t)
+  const b = body()
+  delete b.name
   const resp = await got.post(`http://127.0.0.1:${p}/changesets`, {
     throwHttpErrors: false,
     json: b
-  }).json();
-  t.plan(1);
-  t.is(400, resp.statusCode);
-});
+  }).json()
+  t.plan(1)
+  t.is(400, resp.statusCode)
+})
 
-test("server should 400 on non-string name", async t => {
-  const [p] = await getServer(t);
-  const b = body();
-  b.name = 10;
+test('server should 400 on non-string name', async t => {
+  const [p] = await getServer(t)
+  const b = body()
+  b.name = 10
   const resp = await got.post(`http://127.0.0.1:${p}/changesets`, {
     throwHttpErrors: false,
     json: b
-  }).json();
-  t.plan(1);
-  t.is(400, resp.statusCode);
-});
+  }).json()
+  t.plan(1)
+  t.is(400, resp.statusCode)
+})
 
-test("server should 400 on missing image", async t => {
-  const [p] = await getServer(t);
-  const b = body();
-  delete b.image;
+test('server should 400 on missing image', async t => {
+  const [p] = await getServer(t)
+  const b = body()
+  delete b.image
   const resp = await got.post(`http://127.0.0.1:${p}/changesets`, {
     throwHttpErrors: false,
     json: b
-  }).json();
-  t.plan(1);
-  t.is(400, resp.statusCode);
-});
+  }).json()
+  t.plan(1)
+  t.is(400, resp.statusCode)
+})
 
-test("server should 400 on non-string image", async t => {
-  const [p] = await getServer(t);
-  const b = body();
-  b.image = 10;
+test('server should 400 on non-string image', async t => {
+  const [p] = await getServer(t)
+  const b = body()
+  b.image = 10
   const resp = await got.post(`http://127.0.0.1:${p}/changesets`, {
     throwHttpErrors: false,
     json: b
-  }).json();
-  t.plan(1);
-  t.is(400, resp.statusCode);
-});
+  }).json()
+  t.plan(1)
+  t.is(400, resp.statusCode)
+})
 
-test("server should 400 on missing stakeholders", async t => {
-  const [p] = await getServer(t);
-  const b = body();
-  delete b.stakeholders;
+test('server should 400 on missing stakeholders', async t => {
+  const [p] = await getServer(t)
+  const b = body()
+  delete b.stakeholders
   const resp = await got.post(`http://127.0.0.1:${p}/changesets`, {
     throwHttpErrors: false,
     json: b
-  }).json();
-  t.plan(1);
-  t.is(400, resp.statusCode);
-});
+  }).json()
+  t.plan(1)
+  t.is(400, resp.statusCode)
+})
 
-test("server should 400 on non-array stakeholders", async t => {
-  const [p] = await getServer(t);
-  const b = body();
-  b.stakeholders = 10;
+test('server should 400 on non-array stakeholders', async t => {
+  const [p] = await getServer(t)
+  const b = body()
+  b.stakeholders = 10
   const resp = await got.post(`http://127.0.0.1:${p}/changesets`, {
     throwHttpErrors: false,
     json: b
-  }).json();
-  t.plan(1);
-  t.is(400, resp.statusCode);
-});
+  }).json()
+  t.plan(1)
+  t.is(400, resp.statusCode)
+})
 
-test("server should 400 on non-string-array stakeholders", async t => {
-  const [p] = await getServer(t);
-  const b = body();
-  b.stakeholders.push(10);
+test('server should 400 on non-string-array stakeholders', async t => {
+  const [p] = await getServer(t)
+  const b = body()
+  b.stakeholders.push(10)
   const resp = await got.post(`http://127.0.0.1:${p}/changesets`, {
     throwHttpErrors: false,
     json: b
-  }).json();
-  t.plan(1);
-  t.is(400, resp.statusCode);
-});
+  }).json()
+  t.plan(1)
+  t.is(400, resp.statusCode)
+})
 
-test("server should return uuid on payload", async t => {
-  const [p] = await getServer(t);
-  const b = body();
+test('server should return uuid on payload', async t => {
+  const [p] = await getServer(t)
+  const b = body()
   const resp = await got.post(`http://127.0.0.1:${p}/changesets`, {
     throwHttpErrors: false,
     json: b
-  }).json();
-  t.plan(1);
-  t.not(resp.uuid, undefined, "got uuid back");
-});
+  }).json()
+  t.plan(1)
+  t.not(resp.uuid, undefined, 'got uuid back')
+})
